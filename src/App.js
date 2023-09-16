75f4036839363bbb46157c784f857e63a4c18026
Steph <stephschatz83@gmail.com>
2023-09-16 17:32:24 -0500

    Built html structure of app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
-
+import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+    
+        <Weather />
 
         <footer>
           This project was created by Stephanie Schatz and is{" "}
