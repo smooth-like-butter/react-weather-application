6628006ca0753ab971cea3e35b0c94f4e5b8797d
Steph <stephschatz83@gmail.com>
2023-09-16 17:09:13 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-     
-      <footer>
-        This project was created by Stephanie Schatz and is{" "}
-        <a
-          href="https://github.com/smooth-like-butter/react-weather-application"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced.
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+
+        <footer>
+          This project was created by Stephanie Schatz and is{" "}
+          <a
+            href="https://github.com/smooth-like-butter/react-weather-application"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
