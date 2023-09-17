5a1cbb692ddb43f80cb23b87d89bae49eed75a57
Steph <stephschatz83@gmail.com>
2023-09-17 11:28:15 -0500

    styled app with css and html

--- src/Weather.js
+++ src/Weather.js
@@ -4,44 +4,50 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
-        <form>
-            <div className="row">
-                <div className="col-9">
-            <input type="search"
-            placeholder="Enter city"
-            className="form-control"
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter city"
+              className="form-control"
+              autoFocus="on"
             />
-            </div>
-            <div className="col-3">
-            <input type="submit" value="search" className="btn btn-primary" />
-            </div>
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="search"
+              className="btn btn-primary w-100"
+            />
+          </div>
         </div>
-        </form>
+      </form>
       <h1>New York</h1>
       <ul>
         <li>Wedenesday</li>
         <li>Mostly cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="Partly cloudy"
-          />
-          6°C
+          <div className="clearfix">
+            <div className="float-left">
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                alt="Partly cloudy"
+              />
+
+              <span className="temperature">6</span>
+              <span className="unit">°C</span>
+            </div>
+          </div>
         </div>
         <div className="col-6">
-            <ul>
-                <li>
-                    Precipitation: 15%
-                </li>
-                <li>
-                    Humidity: 72%
-                </li>
-                <li>
-                    Wind: 13 km/h
-                </li>
-            </ul>
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: 72%</li>
+            <li>Wind: 13 km/h</li>
+          </ul>
         </div>
       </div>
     </div>
