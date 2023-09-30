3c3a02a2c25cf4ed0f123c61623d29fbe461c825
Steph <stephschatz83@gmail.com>
2023-09-30 11:48:58 -0500

    built and styled forecast

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -18,7 +18,7 @@ export default function WeatherInfo(props) {
           <div className="clearfix">
             <div className="float-left">
               <WeatherIcon
-                code={props.data.icon}
+                code={props.data.icon} size={52}
               />
               <WeatherTemperature  celsius={props.data.temperature}/>
               
